fcc2585020cbcb17eecd171094d3447621260bf5
spanky-acapulco <martin.mengele@googlemail.com>
2018-04-19 14:35:18 +0200

    Update bookmarklet.js

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,1 +1,15 @@
-alert('hello world!);
+if (!($ = window.jQuery)) { // typeof jQuery=='undefined' works too
+    script = document.createElement( 'script' );
+    script.src = 'src="https://code.jquery.com/jquery-3.3.1.min.js";
+    script.integrity = 'sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=';
+    script.crossorigin = 'anonymous'; 
+    script.onload=runScript;
+    document.body.appendChild(script);
+} 
+else {
+    runScript();
+}
+ 
+function runScript() {
+  alert('hello world);
+}
