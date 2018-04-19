e812b2e657188124876acb654453be9138e3865c
spanky-acapulco <martin.mengele@googlemail.com>
2018-04-19 14:40:30 +0200

    Update bookmarklet.js

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,8 +1,6 @@
-if (!($ = window.jQuery)) { // typeof jQuery=='undefined' works too
+if (!($ = window.jQuery)) {
     script = document.createElement( 'script' );
     script.src = 'https://code.jquery.com/jquery-3.3.1.min.js';
-    script.integrity = 'sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=';
-    script.crossorigin = 'anonymous'; 
     script.onload=runScript;
     document.body.appendChild(script);
 } 
